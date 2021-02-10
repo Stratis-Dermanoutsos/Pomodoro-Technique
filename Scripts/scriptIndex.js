cc4cf4ee5562fc90cea7cdb8a50051a4eba5bcc7
Stratis Dermanoutsos <38103639+Stratis-Dermanoutsos@users.noreply.github.com>
2021-02-10 23:38:44 +0200

    Create repo

---
+++ Scripts/scriptIndex.js
@@ -0,0 +1,46 @@
+/* Add item to the list */
+function AddButton_Click() 
+{
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === "") {
+        alert("Cannot add an empty item!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+
+/* Load the next page with our TODO list */
+function LoadProgram() {
+    /* Save the list to load on the Program page */
+    let myList = GetList();
+    var lis = [];
+    for (const li of myList) {
+        lis.push(li.outerHTML);
+    }
+    sessionStorage.setItem("myList", JSON.stringify(lis));
+
+    window.location.href = "./Program.html"
+}
+
+function GetList() {
+    let content = document.getElementById("myUL").getElementsByTagName('li');
+    console.log(content)
+    return content;
+}
---
+++ Scripts/scriptProgram.js
@@ -0,0 +1,80 @@
+var currentPeriodInSeconds;
+var timesWork = 0;
+var isWorking;
+
+function SetStarted()
+{
+    let now = new Date();
+    document.getElementById("started").innerHTML = 
+    now.getDate() + "/" + (now.getMonth() + 1) + "  " + 
+    now.getHours().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
+    + ":" + 
+    now.getMinutes().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
+    + ":" +
+    now.getSeconds().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+
+    window.LoadList();
+
+    window.StartWork();
+}
+
+function LoadList() {
+    let myList = JSON.parse(sessionStorage.getItem("myList"));
+    var myUl = document.getElementById("myUL");
+    for (const li of myList) {
+        myUl.innerHTML += li;
+    }
+}
+
+function Timer_Tick() {
+    currentPeriodInSeconds--;
+    PrintTime();
+    if (currentPeriodInSeconds == 0) {
+        if (!isWorking)
+            StartBreak();
+        else
+            StartWork();
+    }
+}
+
+function StartWork() {
+    /* Set variables */
+    currentPeriodInSeconds = 1500;
+    timesWork++;
+    isWorking = false;
+
+    document.getElementById("head").innerHTML = "Work period";
+    alert("Time to put in some more work!");
+
+    PrintTime(); // Print
+}
+
+function StartBreak() {
+    /* Set variables */
+    if (timesWork / 4 >= 1 && timesWork % 4 == 0) {
+        currentPeriodInSeconds = 900;
+        alert("Time for a quick break.");
+    } else {
+        currentPeriodInSeconds = 300;
+        alert("Time for a quick break.");
+    }
+    isWorking = true;
+
+    document.getElementById("head").innerHTML = "Break period";
+
+    PrintTime(); // Print
+}
+
+function PrintTime() {
+    let myDiv = parseInt(currentPeriodInSeconds / 60);
+    let myMod = currentPeriodInSeconds % 60;
+    document.getElementById("timer").innerHTML = 
+    myDiv.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
+    + "m " + 
+    myMod.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
+    + "s";
+}
+
+
+window.SetStarted();
+var x = setInterval(Timer_Tick, 1000);
